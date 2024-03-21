aee1666ff7a689d7e0119cb142872f467fa5c49e
babypilot007 <skypirateee@gmail.com>
2024-03-21 18:34:11 -0400

    major Ui changes

--- src/events.js
+++ src/events.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { useEffect, useState } from 'react';
-import {  useNavigate} from 'react-router-dom';
+// import {  useNavigate} from 'react-router-dom';
 import { supabase } from './supabaseClient';
 
 
 const Events = () => {
     
-    const navigate = useNavigate()
+    // const navigate = useNavigate()
     const[data, getData] = useState('')
 
 useEffect(()=>{
@@ -25,23 +25,27 @@ useEffect(()=>{
   
   if(details){
     return (<div className=''> 
-            <p>Please <span> Login</span> or <span>Sign Up</span> to Rsvp</p>
+            <p>Please <span> Login</span> or <span>Sign Up</span> for more Details</p>
             {details.map((info)=>{
 
                 return(
                     <div className='event_hover' key = {info.id}>
                         <div className='event_header'><h1>{info.EventName}</h1> </div>
                         
-                        <h3> Location : {info.EventLocation}</h3>
+                        {/* <h3> Location : {info.EventLocation}</h3> */}
+                        <p>Description :  <br></br>{info.Event_descp}</p>
                        
-                        <p className='rsvp'>Attending : {info.Rsvp}</p>
+                       
 
                     <div className='btn_grp'>
-                        <button className='details' onClick = {()=>{
+                        {/* <button className='details' onClick = {()=>{
                                     console.log(info.id)
                                     navigate('details',{state:info.id})
-                        }}>Details</button> 
+                        }}>Details</button>  */}
+                        <h3 className='rsvp'>Attending : {info.Rsvp}</h3>
+
                         <div>
+                          
                           <h3>Created By:<br />
                           {info.UserName}</h3 ></div>
                      </div>   
