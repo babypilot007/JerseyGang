c888f07e0da4aa65470f4068ad63e1977b42f246
babypilot007 <skypirateee@gmail.com>
2024-04-12 17:11:27 -0400

    event details css

--- src/Components/Rsvp.js
+++ src/Components/Rsvp.js
@@ -246,8 +246,8 @@ const report = async (id)=>{
                         <h1>{info.EventName}</h1>     
                         
                         <WhatsappShareButton 
-                               title= {info.EventName + '\n\n' + info.Event_descp} 
-                               separator={'\n' + info.EventDate + '\n\nMore details : \n'}
+                               title= {info.EventName + '\n\n' + info.Event_descp + '\n'} 
+                               separator={'\n\n' + info.EventDate + '\n\nMore details : \n'}
 
                               
                               url={`https://desigangjc.com/eventdetails/${info.id}`}>
